67e6608dabc0a7f183fecd61c09a7baac1c592db
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-06-24 15:12:57 +0300

    Initial Commit

---
+++ LoadingGallery/LoadingGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingGalleryApp.swift
+//  LoadingGallery
+//
+//  Created by Илья Колесников on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoadingGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
